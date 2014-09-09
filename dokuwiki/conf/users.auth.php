8cf001679f243a2ea797688ac6a78436a8b403fa
admin@csewiki <you@example.com>
2014-09-08 21:54:58 -0400

    ran install.php

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+root:$1$LOYhyZE4$BHGFezqkrExKbukRiNYME0:Mike Wazowski:you@example.com:admin,user
