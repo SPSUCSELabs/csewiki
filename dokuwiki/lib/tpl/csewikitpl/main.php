5b60afd0fb5fd728cbf2959f52295ac79c018a11
theNerd247 <harvey.coder247@gmail.com>
2014-09-18 19:04:53 -0400

    importing new page template

--- dokuwiki/lib/tpl/csewikitpl/main.php
+++ dokuwiki/lib/tpl/csewikitpl/main.php
@@ -1,131 +1,144 @@
-<?php
-/**
- * DokuWiki Twitter Bootstrap Template
- *
- * @link     https://github.com/ryanwmoore/dokutwitterbootstrap
- * @author   Ryan Moore <rwmoore07@gmail.com>
- * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
- */
-
-// error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE); ini_set('display_errors', '1');  // Switch on for error reporting
-
-if (!defined('DOKU_INC')) die(); /* must be run from within DokuWiki */
-@require_once(dirname(__FILE__).'/tpl_functions.php'); /* include hook for template functions */
-
-$showTools = !tpl_getConf('hideTools') || ( tpl_getConf('hideTools') && $_SERVER['REMOTE_USER'] );
-# calling tpl_toc() here returns null if the toc wouldn't normally be rendered
-# so $showTOC will be true if TOC would be rendered, false if not
-# this affects our grid layout later ( see 'if ($showTOC)' )
-$showTOC = ($ACT == "show") && tpl_toc(true);
-
-?><!DOCTYPE html>
-<html lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8" />
-    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
-    <title><?php tpl_pagetitle() ?> [<?php echo strip_tags($conf['title']) ?>]</title>
-    <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
-    <?php tpl_metaheaders() ?>
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
-    <?php tpl_includeFile('meta.html') ?>
-    <link href="<?php echo tpl_getMediaFile(array("css/bootstrap-".tpl_getConf('bootswatch').".min.css")); ?>" rel="stylesheet">
-    <link href="<?php echo tpl_getMediaFile(array("css/modifications.css")); ?>" rel="stylesheet">
-    <script src="<?php echo tpl_getMediaFile(array("js/bootstrap.min.js")); ?>"></script>
-	<script src="<?php echo tpl_getMediaFile(array("js/modifications.js")); ?>"></script>
-	
+	<title>Test page for CSEWiki Design Template</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css"> 
+		<link rel="stylesheet" href="css/modifications.css" type="text/css"> 
+	<meta charset="utf-8" />
 </head>
-
 <body>
-    <?php /* with these Conditional Comments you can better address IE issues in CSS files,
-             precede CSS rules by #IE6 for IE6, #IE7 for IE7 and #IE8 for IE8 (div closes at the bottom) */ ?>
-    <!--[if IE 6 ]><div id="IE6"><![endif]--><!--[if IE 7 ]><div id="IE7"><![endif]--><!--[if IE 8 ]><div id="IE8"><![endif]-->
-
-    <?php 
-	/* 	The "dokuwiki__top" id is needed somewhere at the top, because that's where the "back to top" button/link links to 
-    	classes mode_<action> are added to make it possible to e.g. style a page differently if it's in edit mode,
-        See http://www.dokuwiki.org/devel:action_modes for a list of action modes 
-     	.dokuwiki should always be in one of the surrounding elements (e.g. plugins and templates depend on it) */ ?>
-	<div id="dokuwiki__site">
-		<div id="dokuwiki__top" class="dokuwiki site mode_<?php echo $ACT ?>"></div>
-    	<nav class="navbar navbar-default navbar-fixed-top">
-    		<div class="container">
-				<div class="navbar-header">
-          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-					<a class="navbar-brand" href="./"><?php echo $conf['title']; ?></a>
- 				</div>
-        <div class="collapse navbar-collapse">
-        	<ul class="nav navbar-nav navbar-left">
-						<?php tpl_includeFile('nav.html');?>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Options<b class="caret"></b></a>
-           					<?php _tpl_output_page_tools($showTools, 'li'); ?>
-						</li>
-						<?php _tpl_userinfo(); ?>
-						<li>
-							<div class="navbar-form form-group" role="search">
-								<?php _tpl_output_search_bar(); ?>
-							</div>
-						</li>
-					</ul>
+<header>
+	<nav class="navbar navbar-inverse" role="navigation">
+		<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarcontent">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#"><?php echo $conf['title']; ?></a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="navbarcontent">
+			<div class="navbar-form form-group" role="search">
+				<?php _tpl_output_search_bar(); ?>
+			</div>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+					<?php _tpl_output_page_tools($showTools, 'li'); ?>
+				</li>
+				<li class="dropdown">
+					<?php _tpl_userinfo(); ?>
+				</li>
+			</ul>
+		</div>
+		<!-- /navbarcontent -->
+		</div>
+		<!-- /navbar -->
+	</nav>
+</header>
+<content>
+	<div class="container">
+	<div class="row">
+		<div id="sidebar" class="col-md-3 col-xs-12">
+			<!-- ******************** TOC ******************** -->
+			<div class="row">
+			<div id='toc' class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title" id="toc_title"><a data-toggle="collapse" href="#toc_collapse">Contents</a></h3>
 				</div>
-           	</div> <!-- container -->
-		</nav> <!-- navbar -->
-
-	    <?php html_msgarea() /* occasional error and info messages on top of the page */ ?>
-    	<?php tpl_includeFile('header.html') ?>
-
-        <div class="container">
-        <!-- ********** SIDE BAR for TOCIFY ********** -->
-        	<div class="row">
-				
-				
-				<?php /* when in Show Mode we render the TOC, if not, use full width for content */ 
-				if ($showTOC) { ?>
-					<!-- Make side bar 3 "md's" wide -->
-            		<div class="col-md-3">
-             			<?php _tpl_toc_to_twitter_bootstrap(); ?> 
-           			</div>
-           			<div class="col-md-8" id="dokuwiki__content">
-				<?php } else { ?>
-            		<div class="col-md-11" id="dokuwiki__content">
-				<?php } ?>
-                	<div class="page">
-						<?php tpl_flush(); ?>
-						<?php tpl_content(false); ?>
-						<div class="clearer"></div>
-                    </div>
-				</div>
-			</div><!-- row -->
-        </div><!-- container -->
-
-        <div class="clearer"></div>
-        <hr class="a11y" />
-
-		<!-- ********** FOOTER ********** -->
-		<footer>
-			<div class="clearer"></div>
-			<div class="container">
-				<div class="row">
-        			<div class="col-md-11 text-muted text-right">
-						<?php tpl_flush(); ?>
-            	  		<?php tpl_pageinfo() /* 'Last modified' etc */ ?>
-              			<?php tpl_license('button') /* content license, parameters: img=*badge|button|0, imgonly=*0|1, return=*0|1 */ ?>
-	     	       	  	<?php tpl_includeFile('footer.html') ?>
-    	    		</div>
+				<div id='toc_collapse'class="panel-collapse collapse in">
+					<div class="panel-body">
+						<ul id="toc_top">
+							<li><a href="#s1">1. First Section</a></li>
+							<li><a href="#s2">2. Second Section</a></li>
+							<ul>
+								<li><a href="#s21">2.1 subsection</a></li>
+								<li><a href="#s22">2.2 subsection</a></li>
+							</ul>
+							<li><a href="#s3">3. Third Section</a></li>
+							<ul>
+								<li><a href="#">3.1 subs</a></li>
+								<ul>
+									<li><a href="#">3.1.1 subsection</a></li>
+									<li><a href="#">3.1.2 subsection</a></li>
+									<li><a href="#">3.1.3 subsection</a></li>
+									<li><a href="#">3.1.4 subsection</a></li>
+								</ul>
+							</ul>
+						</ul>
+					</div>
 				</div>
 			</div>
-		</footer>
+			</div>
+			<!-- /TOC -->
+			<div class="row">
+			<div id="news" class="panel panel-default">
+			  <div class="panel-heading">
+			    <h3 class="panel-title"><a data-toggle="collapse" href="#news_collapse">News</a></h3>
+			  </div>
+				<div id='news_collapse' class="panel-collapse collapse in">
+				  <div id="news_content" class="panel-body">
+				    Panel content
+				  </div>
+				</div>
+			</div>
+			</div>
+			<!-- /news -->
+		</div>
+		<!-- /sidebar -->
+		<!-- ******************** Page Content ******************** -->
+		<div id="pagecontainer" class="col-md-9 col-xs-12">
+		<div id="pagecontent">
+			<h1 id="welcome">Welcome to the CSE Wiki</h1>	
+			<hr/>
+			<h2 id="section_1"><a name="s1"></a>Section 1</h2>
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+				tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+				vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+				no sea takimata sanctus est Lorem ipsum dolor sit amet.
+			<h2 id="seciton_2"><a name="s2"></a>Seciton 2</h2>
+				<h3 id="subsection"><a name="s21"></a>subsection</h3>
+					<img src="imghere.png" id="tstimg">
+					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+					tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+					vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+					no sea takimata sanctus est Lorem ipsum dolor sit amet.
+					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+					tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+					vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+					no sea takimata sanctus est Lorem ipsum dolor sit amet.
+				<h3 id="subsection"><a name="s22"></a>subsection</h3>
+					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+					tempor invidunt ut labore et dolore magna aliquyam era.			
+			<h2 id="section_3"><a name="s3"></a>Section 3</h2>
+					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+					tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+					vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+					no sea takimata sanctus est Lorem ipsum dolor sit amet.
+					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+					tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+					vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+					no sea takimata sanctus est Lorem ipsum dolor sit amet.
+		</div>
+		</div>
+		<!-- /Page Content -->
+	</div>
+	</div>
+</content>		
 
-    </div><!-- dokuwiki__site -->
-
-    <div class="no"><?php //tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
-    <!--[if ( IE 6 | IE 7 | IE 8 ) ]></div><![endif]-->
-
+<footer>
+	<div>
+		<br>
+		<br>
+	</div>
+</footer>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-1.11.0.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/modifications.js"></script>
 </body>
 </html>
